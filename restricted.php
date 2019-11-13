2cc54e452f38078f21fd4cc21d50443bda392615
Pol Dellaiera <pol.dellaiera@protonmail.com>
2019-11-13 15:37:44 +0100

    Update.

--- restricted.php
+++ restricted.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 include __DIR__ . '/includes/forceAuthentication.php';
 
